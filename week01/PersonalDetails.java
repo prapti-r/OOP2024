34feeaead4639dd63c140b0d9564ecf3ecaf2a32
prapti-r <prapti.risal12@gmail.com>
2024-02-28 22:27:00 +0545

    week1 added

---
+++ week01/PersonalDetails.java
@@ -0,0 +1,14 @@
+public class PersonalDetails{
+public static void main(String[] args){
+
+String name = "Terry", address = "Sanepa";
+int age = 20;
+long phone = 987654321;
+
+System.out.println("Name: "+ name);
+System.out.println("Address: "+ address);
+System.out.println("Age: "+ age);
+System.out.println("Phone number: "+ phone);
+
+}
+}
